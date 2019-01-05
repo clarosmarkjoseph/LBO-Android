039f68f1b2448752bebcdc655244a4a49105388c
nhOi <clarosmarkjoseph@gmail.com>
2019-01-05 18:09:11 +0800

    Update master repository

---
+++ app/build/generated/source/buildConfig/release/com/system/mobile/lay_bare/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.system.mobile.lay_bare;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.system.mobile.lay_bare";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 25;
+  public static final String VERSION_NAME = "12.7";
+}
